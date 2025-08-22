3ca64fab427f6f1f663b95822731dbf56e824538
mis-ethan <ethan.green@ochsinc.org>
2025-08-22 16:53:57 -0700

    added correct? headers

--- main.py
+++ main.py
@@ -39,11 +39,13 @@ async def get_group_items():
     """
     variables = {"board_id": board_id, "group_id": group_id}
 
+    headers = {'Authorization': MONDAY_API_TOKEN}
+
     async with httpx.AsyncClient() as client:
         response = await client.post(
             MONDAY_API_URL,
             json={"query": query, "variables": variables},
-            headers=HEADERS,
+            headers,
         )
 
     if response.status_code != 200:
