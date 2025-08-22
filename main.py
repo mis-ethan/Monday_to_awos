9fd88ed46c172be8f1cb4b686b44b419dd8fc8ac
mis-ethan <ethan.green@ochsinc.org>
2025-08-22 14:57:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 from fastapi import FastAPI
-const port = process.env.PORT || 4000 
+
 
 
 app = FastAPI()
 
+port = process.env.PORT || 4000 
 def read_root():
     return {"message": "Welcome to my API"}
