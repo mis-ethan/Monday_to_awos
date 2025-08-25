ea84d805d11b440403184741d500cf1d3c2b0b31
mis-ethan <ethan.green@ochsinc.org>
2025-08-25 08:55:13 -0700

    troubleshooting parsing function

--- main.py
+++ main.py
@@ -57,6 +57,7 @@ async def get_group_items():
     data = response.json()
     try:
         items = data["data"]["boards"][0]["groups"][0]["items"]
+        return items
         result = []
         for item in items:
             result.append({
